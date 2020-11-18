928598585c93fdce054cd0fa1953eceae94960ce
miralda <miraldalexandra15@gmail.com>
2020-11-18 11:00:07 +0100

    add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../style/footer.css'
+
+export default function Footer() {
+    return (
+        <footer>
+            <p>Built with &#10084; by Natalia</p>
+        </footer>
+    )
+}
